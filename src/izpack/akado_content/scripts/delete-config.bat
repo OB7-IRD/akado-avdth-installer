9e67c8f5e0b8645cc97b3a77ddf4a5ce5a7a75c8
Julien Lebranchu <julien.lebranchu@ird.fr>
2016-04-19 16:39:03 +0200

    MAJ de la documentation+donnée GIS+Passage de RC7  à 2.0.0 pour le GTT3

--- src/izpack/akado_content/scripts/delete-config.bat
+++ src/izpack/akado_content/scripts/delete-config.bat
@@ -1,2 +1,3 @@
 @echo Suppression du repertoire de configuration
+
 rmdir /S /Q %userprofile%\appconfig\akado-avdth
--- src/izpack/akado_content/scripts/delete-config.ps1
+++ src/izpack/akado_content/scripts/delete-config.ps1
@@ -1,2 +1,3 @@
 echo "Suppression du repertoire de configuration"
+
 rm -force $env:userprofile\appconfig\akado-avdth
